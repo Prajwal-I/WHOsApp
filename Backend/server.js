252957b0515a515045f08144e3b5d5e41562a46c
Mohd Taha Sharjeel <tahasharjeel@gmail.com>
2021-05-08 16:38:21 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/WHOsAppDataBase', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
+const db = mongoose.connection;
 
 const PORT = process.env.PORT || 2400;
 
-app.listen(PORT, () => console.log('server started on port ' + PORT))
+app.listen(PORT, () => console.log('server started on port ' + PORT));
